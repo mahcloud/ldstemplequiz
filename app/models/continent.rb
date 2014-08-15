d56e2df5cfc6f81bec5b69da0508ef441a562d9e
mahcloud <mike.hoffert@gmail.com>
2014-08-15 00:17:26 -0600

    Don't show continents with temples

--- app/models/continent.rb
+++ app/models/continent.rb
@@ -3,4 +3,8 @@ class Continent < ActiveRecord::Base
   has_many :temples, :through => :countries
 
   validates :name, :presence => true, :uniqueness => true
+
+  def has_temples?
+    temples.count > 0
+  end
 end
--- spec/models/continent_spec.rb
+++ spec/models/continent_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Continent, :type => :model do
   it { should have_many(:temples) }
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+
+  describe "#has_temples?" do
+    let(:continent) { Fabricate(:continent) }
+
+    context "has temples" do
+      let!(:country) { Fabricate(:country, :continent_id => continent.id) }
+      before(:each) do
+        Fabricate(:temple, :country_id => country.id)
+      end
+
+      it "should be true" do
+        continent.has_temples?.should == true
+      end
+    end
+
+    context "no temples" do
+      it "should be false" do
+        continent.has_temples?.should == false
+      end
+    end
+  end
 end
