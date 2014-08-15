2c6c19654c49484577dca717c498d4ec31e48458
mahcloud <mike.hoffert@gmail.com>
2014-08-15 14:44:23 -0600

    Fix temples controller changes in spec

--- spec/controllers/temples_controller_spec.rb
+++ spec/controllers/temples_controller_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 
 describe TemplesController, type: :controller do
   let!(:continent) { Fabricate(:continent) }
+  let!(:temple) { Fabricate(:temple, :name => 'def', :country_id => Fabricate(:country, :continent_id => continent.id).id) }
+  let!(:find_continent) { Fabricate(:continent) }
+  let!(:find_temple) { Fabricate(:temple, :name => 'abc', :country_id => Fabricate(:country, :continent_id => find_continent.id).id) }
 
   describe "GET index" do
     it "it should assign continent" do
       get :index, :format => 'json'
-      assigns(:continent).should == continent
+      assigns(:temples).count.should == 2
+      assigns(:temples).should == [find_temple, temple]
     end
 
     it "it should find continent" do
-      find_continent = Fabricate(:continent)
       get :index, :continent_id => find_continent.id, :format => 'json'
-      assigns(:continent).should == find_continent
+      assigns(:temples).should == [find_temple]
     end
   end
 end
