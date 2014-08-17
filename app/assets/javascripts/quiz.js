74959acf515895469aa1dc4f48e591cfe88f58cd
mahcloud <mike.hoffert@gmail.com>
2014-08-17 17:16:06 -0600

    Hide temple answer bar until shown

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -2,17 +2,19 @@ $(document).ready(function() {
 	maplace.o.afterCreateMarker = function (index, location, marker) {
 	    google.maps.event.addListener(marker, 'click', function() {
               $.post('/answers.json', { temple_id: this.id }, function(data) {
+                $('#gmap, .accordion').hide();
+                if(data['temple_name'] == 'Already guessed') {
+		  $('#temple_answer').html('You have already guessed!');
+                } else {
+                  $('#temple_answer').html(data['temple_name'] + ' Temple');
+                }
+		$('#temple_answer').show();
                 if(data['correct'] === true){
                   alert('Correct');
                 } else if(data['temple_name'] == 'Already guessed') {
                   alert('You have already guessed. Please take the quiz again.');
                 } else {
                   alert('Incorrect! It was the '+data['temple_name']+' Temple!');
-                }
-                $('#gmap, .accordion').hide();
-                if(data['temple_name'] == 'Already guessed') {
-                } else {
-                  $('#temple_answer').html(data['temple_name'] + ' Temple');
                 }
               });
 	    });
